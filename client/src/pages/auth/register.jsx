625ca625fbf8e9dbb97d85c3f5af390f0a8b3e58
RahmanAdhitya <rahman.adhitya23@gmail.com>
2022-04-21 14:37:00 +0700

    FE register

---
+++ client/src/pages/auth/register.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Box, Flex, FormControl, FormHelperText, FormLabel, Input, Stack } from '@chakra-ui/react';
+import { useFormik } from 'formik';
+
+const register = () => {
+  const formik = useFormik({
+    initialValuse: {
+      username: '',
+      email: '',
+      password: '',
+    },
+  });
+
+  return (
+    <Flex>
+      <Box>
+        <Stack>
+          <FormControl isInvalid={formik.errors.username}>
+            <FormLabel htmlFor="inputUsername">Username</FormLabel>
+            <Input onChange={inputHandler} id="inputUsername" name="username" />
+            <FormHelperText>{formik.errors.username}</FormHelperText>
+          </FormControl>
+
+          <FormControl isInvalid={formik.errors.email}>
+            <FormLabel htmlFor="inputEmail">Email</FormLabel>
+            <Input onChange={inputHandler} id="inputEmail" name="email" />
+            <FormHelperText>{formik.errors.email}</FormHelperText>
+          </FormControl>
+
+          <FormControl isInvalid={formik.errors.password}>
+            <FormLabel htmlFor="inputPassword">Password</FormLabel>
+            <Input onChange={inputHandler} id="inputPassword" name="password" />
+            <FormHelperText>{formik.errors.password}</FormHelperText>
+          </FormControl>
+        </Stack>
+      </Box>
+    </Flex>
+  );
+};
+
+export default register;
---
+++ client/src/redux/reducers/auth.js
@@ -0,0 +1,9 @@
+const init_state = {
+  id: 0,
+  username: '',
+  email: '',
+};
+
+export const auth_reducer = (state = init_state) => {
+  return state;
+};
