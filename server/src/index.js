7c4112c82bd43379ebb7ed06d6ae6b904d22bea7
RahmanAdhitya <rahman.adhitya23@gmail.com>
2022-04-22 13:08:42 +0700

    register user work in postman

--- server/src/index.js
+++ server/src/index.js
@@ -1,22 +1,26 @@
-const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const app = express();
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
 dotenv.config();
 
 const PORT = process.env.PORT;
 
-const { sequelize } = require("./lib/sequelize");
-
+const { sequelize } = require('./lib/sequelize');
 sequelize.sync({ alter: true });
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("<h1>E-Commerce</h1>");
+const { authRoutes } = require('./routes');
+
+app.use('/auth', authRoutes);
+
+app.get('/', (req, res) => {
+  res.send('<h1>E-Commerce</h1>');
 });
 
 app.listen(PORT, () => {
-  console.log("Listening in port", PORT);
+  console.log('Listening in port', PORT);
 });
